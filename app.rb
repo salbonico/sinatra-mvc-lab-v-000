af3a6de1c2acb862e7144cf5897d5ec010eda04a
Steve Albonico <salbonico@gmail.com>
2019-02-06 02:02:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+erb :user_input
+end
+
+
+post '/' do
+
+@text = PigLatinizer.new
+@piglatin = @text.piglatinize(params[:input])
+
+  erb :piglatinized
+end
+
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,36 @@
+class PigLatinizer
+  attr_accessor :sentence
+
+  def initialize
+
+end
+
+
+def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u", "I"].include?(first_letter)
+        "#{word}way"
+    else
+      consonants = []
+    consonants << word[0]
+      if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants << word[1]
+        if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+          consonants << word[2]
+        end
+      end
+    "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
+end
+
+
+ def piglatinize(string)
+   a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+ end
+
+
+
+
+ end
